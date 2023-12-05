a015be6c99689f5b9a3881670bf0960af570b8e6
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-05 21:02:10 +0100

    fix: mostrare lista 10 elementi con pulsante

--- main.js
+++ main.js
@@ -8,9 +8,19 @@ createApp({
         'apiUru':'https://flynn.boolean.careers/exercises/api/random/mail',
         'mail': '',
         'mailArr': [],
+        'showMailClick': false,
+        'notShowMail': null,
     };
   },
   methods:{
+    // funzione mostra lista e chiudi lista 
+    showMail(){
+        this.showMailClick = !this.showMailClick;
+        this.notShowMail += 1;
+        if(this.notShowMail === 2){
+            this.showMailClick = false;
+        }
+    }
   },
   created(){
     for(let i = 0; i < 10; i++){
