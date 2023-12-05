1f16125f234c51ac44f068c04d6716e7ac152af4
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-05 17:17:36 +0100

    fix: prendo api con axios e restituisco contanuto in console

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-    }
+        'apiUru':'https://flynn.boolean.careers/exercises/api/random/mail',
+        'mail': '',
+    };
+  },
+  created(){
+    axios.get(this.apiUru).then((response)=>{
+        console.log(response.data);
+        this.mail = response.data.response;
+    });
   }
 }).mount('#app')
